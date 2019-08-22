8a6ba010e3cee556703628fe7360e29371f0b204
Matej Fandl <fandl@mladypes.sk>
2019-08-22 14:49:59 +0200

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -6,48 +6,37 @@ import System.Random
 import Linear
 
 import Types
-import qualified Particles as P1
+import Particles
 
-import qualified Particles2 as P2
-
-import qualified Data.Sequence as S (Seq, empty, (<|), fromList)
-import Data.Foldable (toList)
-
-type World = S.Seq Particle
+type World = [Particle]
 
 displayMode = InWindow "Window" (400, 400) (0, 0)
-
-fct :: (Num a, Enum a) => a -> a
-fct n = product [1..n]
-
-oo :: (Integral a, Enum a) => a -> a -> a
-oo m n = (fct m) `div` ((fct n) * (fct (m - n)))
-
 
 shape :: Picture
 shape = Circle 2
 
 drawFunc :: World -> Picture
-drawFunc = pictures . toList . fmap drawParticle
+drawFunc = pictures . fmap drawParticle
   where
     drawParticle (Particle ((V2 x y), _)) = translate x y shape
 
 updateFunc :: ViewPort -> Float -> World -> World
-updateFunc _ dt = fmap updatePosition . P2.mkIntegrateForces(2) dt
+updateFunc _ dt = map updatePosition . integrateForces dt
   where
     updatePosition (Particle (pos, vel)) = Particle (pos + vel ^* dt, vel)
 
 count :: Int
-count = 70
+count = 200
+
 
 main :: IO ()
 main = do
   g <- getStdGen
   let rnds = take (4 * count) $ (randoms g :: [Float])
-  simulate displayMode white 60 duetWorld drawFunc updateFunc
+  simulate displayMode white 60 (initialWorld rnds) drawFunc updateFunc
     where
-      initialWorld [] = S.empty
-      initialWorld (x:y:vx:vy:rs) = Particle ((V2 (-200 + 400 * x) (-200 + 400 * y)), (V2 vx vy)) S.<| (initialWorld rs)
-      duetWorld = S.fromList [Particle ((V2 0 0), (V2 0 0)), Particle ((V2 10 0), (V2 0 10))]
+      initialWorld [] = []
+      initialWorld (x:y:vx:vy:rs) = Particle ((V2 (-200 + 400 * x) (-200 + 400 * y)), (V2 vx vy)) : (initialWorld rs)
+      duetWorld = [Particle ((V2 0 0), (V2 0 0)), Particle ((V2 10 0), (V2 0 10))]
 
   
